98459912113615f0ed0e1078109a9e3a152a138a
Pradnya Bhagwat <pradnya144@gmail.com>
2022-04-03 15:13:31 +0000

    Add text colours class

--- run.py
+++ run.py
@@ -1,9 +1,21 @@
 import json
 
-f = open('words.json')
-data = json.load(f)
+class text_colours:
+    GREEN = '\u001b[32;1m'
+    WHITE = '\033[0m'
+    RED = '\u001b[31m'
+    BLUE = '\u001b[34m'
+    YELLOW = '\u001b[33m'
+    MAGENTA = '\u001b[35m'
 
-for i in data['data']:
-    print(i)
 
-f.close()
+print(text_colours.YELLOW + "Hello" + text_colours.MAGENTA + " World")
+
+
+#f = open('words.json')
+#data = json.load(f)
+
+#for i in data['data']:
+#    print(i)
+
+#f.close()
